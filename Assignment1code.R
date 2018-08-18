f6278ac0aaafe7574b83c48113a9301471907b1a
Badi James <badi.t.james@gmail.com>
2018-08-18 22:27:04 +1200

    Finished A1

--- Assignment1code.R
+++ Assignment1code.R
@@ -12,10 +12,12 @@ realPhenFreqO <- nO / N
 # Takes a vector of initial allele frequency guesses (pInit) and observed
 # phenotype counts (nA, nB, nAB, nO)
 emBlood <- function(pInit, nA, nB, nAB, nO) {
+  if(sum(pInit) != 1) stop("Initial allele frequencies do not sum to 1")
   pA <- pInit[1]
   pB <- pInit[2]
   pO <- pInit[3]
   N <- nA +nB + nAB + nO
+  count <- 1
   while(TRUE){
     nAO <- 2* pA * pO * N
     nAA <- nA - nAO
@@ -30,19 +32,19 @@ emBlood <- function(pInit, nA, nB, nAB, nO) {
       pA <- newPA
       pB <- newPB
       pO <- newPO
+      count <- count + 1
     } else {
       break
     }
   }
-  alleleFreq <- c(pA, pB, pO)
-  names(alleleFreq) <- c("pA", "pB", "pO")
+  alleleFreq <- c(pA, pB, pO, count)
+  names(alleleFreq) <- c("pA", "pB", "pO", "Iterations until Convergence")
   return(alleleFreq)
 }
 
 # Create 50 random sets of initial allele frequencies
 # Use to test if different inputs result in finding different local maxima
 startfreq <- matrix(nrow = 50, ncol = 3)
-
 for(i in 1:50){
   ps <- c(0,0,0)
   place <- sample(1:3, 3)
@@ -52,21 +54,35 @@ for(i in 1:50){
   ps[place[3]] <- 1-max(bar)
   startfreq[i,] <- ps
 }
+# Check distribution of starting frequencies
+# hist(startfreq[,2]) 
 
-hist(startfreq[,2])
-
-maxLAlleleFreq <- apply(startfreq, 1, emBlood, nA = nA, nB = nB, nO = nO, nAB = nAB)
+# Run EM on all input sets. Tally each output value for each frequency
+maxLAlleleFreq <- apply(startfreq, 1, emBlood, 
+                        nA = nA, nB = nB, nO = nO, nAB = nAB)
 maxLAlleleFreq <- t(maxLAlleleFreq)
-freqCounts <- list(table(maxLAlleleFreq[,1]), table(maxLAlleleFreq[,2]), table(maxLAlleleFreq[,3]))
-names(freqCounts) <- colnames(maxLAlleleFreq)
+freqCounts <- list(table(maxLAlleleFreq[,1]), table(maxLAlleleFreq[,2]), 
+                   table(maxLAlleleFreq[,3]))
+names(freqCounts) <- colnames(maxLAlleleFreq[,1:3])
+# Check median and mean times through loop untill convergence
+medianRuns <- median(maxLAlleleFreq[,4])
+meanRuns <- mean(maxLAlleleFreq[,4])
 
+# See how phenotype frequencies calculated from an EM allele frequency
+# estimate compares to the observed phenotype frequencies
 pA <- maxLAlleleFreq[1,1]
 pB <- maxLAlleleFreq[1,2]
 pO <- maxLAlleleFreq[1,3]
-nAEst <- (2* pA * pO + pA**2) * N
-nBEst <- (2* pB * pO + pB**2) * N
-nABEst <- 2 * pA * pB * N
-nOEst <- pO**2 * N
+fAEst <- 2* pA * pO + pA**2
+fBEst <- 2* pB * pO + pB**2
+fABEst <- 2 * pA * pB
+fOEst <- pO**2
+
+estimated <- c(fAEst, fBEst, fABEst, fOEst)
+observed <- c(realPhenFreqA, realPhenFreqB, realPhenFreqAB, realPhenFreqO)
+comparison <- cbind(estimated, observed)
+row.names(comparison) <- c('A', 'B', 'AB', 'O')
+
 
 
 
